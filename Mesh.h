4815feb94fdb2f99204a868c3bb832212a28dad1
aksuashkin <167909073+aksuashkin@users.noreply.github.com>
2024-11-28 16:37:07 +0300

    Create Mesh.h

---
+++ Mesh.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include "GL/freeglut.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+
+class Mesh
+{
+private:
+	struct Vertex
+	{
+		glm::vec3 pos;
+		glm::vec3 normal;
+		glm::vec2 texPos;
+		//GLfloat pos[3]; 12
+		//GLfloat normal[3]; 12
+		//GLfloat texPos[2]; 8
+	};
+	std::vector<Vertex> vertices;
+public:
+	Mesh();
+	Mesh(std::string sourcePath);
+	void LoadFromFile(std::string sourcePath);
+	void Draw();
+};
